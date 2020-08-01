2ee89863e9c449fdf47f3a00958df6c937fa1c68
Ankan <ankanm46@gmail.com>
2020-08-01 11:08:16 +0530

    Logistic Model Beautified

--- App/server.R
+++ App/server.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6af0a8ac1da1f4a51ca9f5118a610e7acdf40e1e02e3e9c89bdb7b9cc39ebb3
-size 10572
+oid sha256:20dc91e50280ae7445a91a7c6ef98ff5295c9b4f96b26610b3baeb5898b9e1de
+size 10698
